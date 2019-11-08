9c6761049b3a93713ee0a0153af4e8b81c004f28
Tuna Cinsoy <tunacinsoy@gmail.com>
2019-11-08 14:07:36 +0000

    Updated Game.java

--- Game.java
+++ Game.java
@@ -1,18 +1,37 @@
+// 07.11.2019
 import java.util.ArrayList;
 
 public class Game {
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private Board board;
 	private int initialMoney;
-	private static int howManyTaxedCells;
+	private int howManyTaxedCells;
 	private int taxedMoneyAmount;
 	private Bank bank ;
-	private Dice dice;
 
+	Game(int initialMoney, int howManyTaxedCells, String[] playerNames, int taxedMoneyAmount) {
+		this.initialMoney = initialMoney;
+		this.taxedMoneyAmount = taxedMoneyAmount;
+		this.howManyTaxedCells = howManyTaxedCells;
+		bank = new Bank();
 
+		for(int i = 0; i < playerNames.length; i++){
 
-	Game() {
-		//System.out.println("taxed cell no :"+howManyTaxedCells);
+			players.add(new Player(playerNames[i],20,initialMoney,i));
+			if(i == playerNames.length-1){players.get(i).setNextTurn(0);continue;}
+			players.get(i).setNextTurn(i+1);
+		}
+		//board.assignAllPieces(players);
+		board = new Board(howManyTaxedCells,playerNames.length, taxedMoneyAmount, players);
+		for(int i  = 0; i < playerNames.length; i++){
+			players.get(i).setPiece(board.getPieces()[i]);
+		}
+
+		System.out.println("Players:");
+		for (int i = 0; i < players.size(); i++) {
+			System.out.println( (i+1) +". " +  players.get(i).getName());
+		}
+		System.out.println();
 
 	}
 
@@ -28,69 +47,11 @@ public class Game {
 
 
 	public void Play(){
-		bank = new Bank();
-		players.get(0).onTurn();
-
-	}
-
-	public ArrayList<Player> getPlayers() {
-		return players;
-	}
-	public void setPlayers(ArrayList<Player> players) {
-		this.players = players;
-	}
-	public Board getBoard() {
-		return board;
-	}
-	public void setBoard(Board board) {
-		this.board = board;
-	}
-	public int getInitialMoney() {
-		return initialMoney;
-	}
-	public void setInitialMoney(int initialMoney) {
-		this.initialMoney = initialMoney;
-	}
-	public static int getHowManyTaxedCells() {
-		return howManyTaxedCells;
-	}
-	public void setHowManyTaxedCells(int howManyTaxedCells) {
-		this.howManyTaxedCells = howManyTaxedCells;
-		//System.out.println("----" + howManyTaxedCells + "----");
-	}
-	public Bank getBank() {
-		return bank;
-	}
-	public void setBank(Bank bank) {
-		this.bank = bank;
-	}
-	public void setNames(String playerNames[]){
-		for(int i = 0; i < playerNames.length; i++){
-
-			players.add(new Player(playerNames[i],20,initialMoney,i));
-			if(i == playerNames.length-1){players.get(i).setNextTurn(0);continue;}
-			players.get(i).setNextTurn(i+1);
-		}
-		System.out.println("setnames'deyim");
-		//board = new Board();
-		board.fillTheBoard();
-		for(int i  = 0; i < playerNames.length; i++){
-			players.get(i).setPiece(board.getPieces()[i]);System.out.println(i+"inci");
-		}
-	}
-
-
-	public Dice getDice() {
-		return dice;
-	}
-
-	public void createBoard(){
-		Board board = new Board();
-		this.board = board;
-	}
+		players.get(0).onTurn(board.getDice(),board.getCells(),players,bank);
 
 
 
+	}
 
 
 
