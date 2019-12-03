493b26fe1604192828ca4b788e0c6d07d759d5d8
Ahmet Enes GÜNDÜZ <aegunduz69@gmail.com>
2019-12-03 20:15:45 +0000

    Upload New File

---
+++ Card.java
@@ -0,0 +1,40 @@
+//03.12.2019
+public abstract class Card {
+	
+	private String nameOfTheCard;
+	private String actionOfTheCard;
+	private int cardID;
+	
+	Card()
+	{
+		
+	}
+
+	public String getNameOfTheCard() {
+		return nameOfTheCard;
+	}
+
+	public void setNameOfTheCard(String nameOfTheCard) {
+		this.nameOfTheCard = nameOfTheCard;
+	}
+
+	public String getActionOfTheCard() {
+		return actionOfTheCard;
+	}
+
+	public void setActionOfTheCard(String actionOfTheCard) {
+		this.actionOfTheCard = actionOfTheCard;
+	}
+
+	public int getCardID() {
+		return cardID;
+	}
+
+	public void setCardID(int cardID) {
+		this.cardID = cardID;
+	}
+	
+	
+	
+
+}
