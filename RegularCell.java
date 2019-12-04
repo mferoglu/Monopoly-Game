7aea02ddbcab5162a5cf52738e2ad67c0b9767a2
Tuna Cinsoy <tunacinsoy@gmail.com>
2019-12-04 20:46:08 +0000

    Replace RegularCell.java

--- RegularCell.java
+++ RegularCell.java
@@ -1,12 +1,13 @@
-// 04.12.2019
+// 07.11.2019
 public class RegularCell extends Cell {
-	private String name = "Regular cell";
+	private String name = "Free Car Park";
 
 	RegularCell(int id, Player owner){
 		super(id,owner);
+		super.setCanBeBought(false);
 	}
 	public void MoneyFunc(Player player,Bank bank){
-		System.out.println("Nothing will happen in this cell !");
+		System.out.println("User can relax in this cell! VACATION TIME!");
 		// Will be implemented in subclass of Cell -> "PayMoneyCell"
 	}
 	public String getName() {
