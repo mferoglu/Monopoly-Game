a1d205d6442178ab75a675dadb67259dd8ec462c
Ahmet Enes GÜNDÜZ <aegunduz69@gmail.com>
2019-12-27 19:50:17 +0000

    Replace Cell.java

--- Cell.java
+++ Cell.java
@@ -1,10 +1,9 @@
-// 04.12.2019
+// 27.12.2019
 public abstract class Cell {
 
 	private int cellId;
 	private Player owner;
 	private boolean canBeBought = true;
-
 
 	Cell(int id, Player owner) {
 		this.cellId = id;
@@ -19,8 +18,6 @@ public abstract class Cell {
 	public void setCellId(int cellId) {
 		this.cellId = cellId;
 	}
-
-
 
 	public Player getOwner() {
 		return owner;
@@ -37,15 +34,20 @@ public abstract class Cell {
 	public void setCanBeBought(boolean canBeBought) {
 		this.canBeBought = canBeBought;
 	}
+
 	public boolean getCanBeBought() {
 		return this.canBeBought;
 	}
 
 	public abstract String getName();
-	public void MoneyFunc(Player player,Bank bank) {}
+
+	public void MoneyFunc(Player player, Bank bank) {
+	}
+
 	public ChanceCard[] getChanceCards() {
 		return null;
 	}
+
 	public CommunityCard[] getCommunityCards() {
 		return null;
 	}
@@ -53,18 +55,26 @@ public abstract class Cell {
 	public int getRent() {
 		return 1;
 	}
+
 	public String getColor() {
 		return null;
 	}
-	public boolean buyingCell(Player person){
+
+	public boolean buyingCell(Player person) {
 		return false;
 
 	}
-	public void payRent(Player player){}
-	public void increaseBuildingNumber(){}
+
+	public void payRent(Player player) {
+	}
+
+	public void increaseBuildingNumber() {
+	}
+
 	public int getPriceOfTheCell() {
 		return 0;
 	}
+
 	public int getCountOfBuildings() {
 		return 0;
 	}
